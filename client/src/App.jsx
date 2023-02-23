c376fa532061fa095d6926a41c8aeda2107c9039
William Salas <willbullet52@gmail.com>
2023-02-23 18:12:24 -0500

    added router dom and modified heading

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,3 +1,4 @@
+import { Heading } from "@chakra-ui/react";
 import { useState } from "react";
 
 import "./styles/App.css";
@@ -5,7 +6,7 @@ import "./styles/App.css";
 function App() {
 	return (
 		<div className="App">
-			<h2>Stock Managment App</h2>
+			<Heading className="heading">Stock Management App</Heading>
 		</div>
 	);
 }
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import { BrowserRouter as Router } from "react-router-dom";
+import { ChakraProvider } from "@chakra-ui/react";
 import "./styles/index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
 	<React.StrictMode>
-		<App />
+		<Router>
+			<ChakraProvider>
+				<App />
+			</ChakraProvider>
+		</Router>
 	</React.StrictMode>
 );
