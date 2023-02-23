c045f54ff0240bd2408eddeed2b918619067e61b
William Salas <willbullet52@gmail.com>
2023-02-23 17:30:28 -0500

    first commit

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+
+import "./styles/App.css";
+
+function App() {
+	return (
+		<div className="App">
+			<h2>Stock Managment App</h2>
+		</div>
+	);
+}
+
+export default App;
